4e4f062ac4dfb1236e4e43ca3e5bc9d91604f2ea
Jiri Kuncar <jiri.kuncar@cern.ch>
2015-08-31 11:50:21 +0200

    global: port of /ping endpoint
    * Ports /ping endpoint used by HAProxy load balancer.  (closes #3471)
    
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2014 CERN.
+# Copyright (C) 2014, 2015 CERN.
 #
 # Invenio is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -19,7 +19,13 @@
 
 """Define base Blueprint."""
 
-from flask import Blueprint
+from flask import Blueprint, Response
 
 blueprint = Blueprint('base', __name__, template_folder='templates',
                       static_folder='static')
+
+
+@blueprint.route('/ping')
+def ping():
+    """Response with health status."""
+    return Response('OK', mimetype='text/plain')
