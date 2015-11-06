62e75e374d190242061713c8afbd56f5c85fc3b6
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2015-11-06 13:11:56 +0100

    app: forced static folder fix
    * Fixes problem with static endpoint also being installed on the API
      Flask application. (closes #36) (addresses #33)
    
    * Moves default instance and static folder paths to cookiecutter
      template. Existing installations must update their application
      factories.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio_base/cookiecutter-invenio-base/{{ cookiecutter.site_name }}/{{ cookiecutter.package_name }}/factory.py
+++ invenio_base/cookiecutter-invenio-base/{{ cookiecutter.site_name }}/{{ cookiecutter.package_name }}/factory.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""Mysite Invenio API application."""
+"""{{ cookiecutter.site_name }} application factories."""
+
+import os
+import sys
 
 from invenio_base.app import create_app_factory
 from invenio_base.wsgi import create_wsgi_factory
@@ -12,12 +15,29 @@ env_prefix = 'APP'
 
 conf_loader = create_conf_loader(config=config, env_prefix=env_prefix)
 
+instance_path = os.getenv(env_prefix + '_INSTANCE_PATH') or \
+    os.path.join(sys.prefix, 'var', '{{ cookiecutter.package_name }}-instance')
+"""Instance path for Invenio.
+
+Defaults to ``<env_prefix>_INSTANCE_PATH`` or if environment variable is not
+set ``<sys.prefix>/var/<app_name>-instance``.
+"""
+
+static_folder = os.getenv(env_prefix + '_STATIC_FOLDER') or \
+    os.path.join(instance_path, 'static')
+"""Static folder path.
+
+Defaults to ``<env_prefix>_STATIC_FOLDER`` or if environment variable is not
+set ``<sys.prefix>/var/<app_name>-instance/static``.
+"""
+
 create_api = create_app_factory(
     '{{ cookiecutter.package_name }}',
     env_prefix=env_prefix,
     conf_loader=conf_loader,
     bp_entry_points=['invenio_base.api_blueprints'],
     ext_entry_points=['invenio_base.api_apps'],
+    instance_path=instance_path,
 )
 
 
@@ -27,5 +47,7 @@ create_app = create_app_factory(
     conf_loader=conf_loader,
     bp_entry_points=['invenio_base.blueprints'],
     ext_entry_points=['invenio_base.apps'],
-    wsgi_factory=create_wsgi_factory({'/api': create_api})
+    wsgi_factory=create_wsgi_factory({'/api': create_api}),
+    instance_path=instance_path,
+    static_folder=static_folder,
 )
