783ae1d05146b3b1cd141ff9178cf3c66034ba75
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-09-29 15:04:20 +0200

    tests: doctest invocation problem hotfix
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -26,5 +26,5 @@ pydocstyle invenio_base && \
 isort -rc -c -df **/*.py && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+sphinx-build -qnNW -b doctest docs docs/_build/doctest && \
+python setup.py test
