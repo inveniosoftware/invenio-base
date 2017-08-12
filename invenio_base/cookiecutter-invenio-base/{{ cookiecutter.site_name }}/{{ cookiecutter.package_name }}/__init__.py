9c8a923a697509d6f2aece0869d2d5009f6b22ed
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-08-12 10:31:39 +0200

    global: cookiecutter template removal
    * Removes cookiecutter template in favor of standalone template.

--- invenio_base/cookiecutter-invenio-base/{{ cookiecutter.site_name }}/{{ cookiecutter.package_name }}/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""{{ cookiecutter.site_name }}."""
-
-from __future__ import absolute_import, print_function
-
-from .version import __version__
-
-__all__ = ('__version__',)
--- invenio_base/cookiecutter-invenio-base/{{ cookiecutter.site_name }}/{{ cookiecutter.package_name }}/wsgi.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""{{ cookiecutter.site_name }} Invenio WSGI application."""
-
-from __future__ import absolute_import, print_function
-
-from .factory import create_app
-
-application = create_app()
