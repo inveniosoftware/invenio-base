d12d883a0f7aec27bd528a5998c59b8bbf78cf26
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-11-19 11:28:15 +0100

    release: v2.0.0

--- invenio_base/__init__.py
+++ invenio_base/__init__.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2024 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -255,7 +256,7 @@ except AttributeError:
 
     security.safe_str_cmp = hmac.compare_digest
 
-__version__ = "1.4.0"
+__version__ = "2.0.0"
 
 __all__ = (
     "__version__",
