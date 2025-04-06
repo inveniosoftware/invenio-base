56969f2ddbe17311dae1ebd1a44ed6c107b913c5
Laura <lthaci@tugraz.at>
2025-04-06 18:50:48 +0200

    signals: add type annotations for signals
    * adds type annotations for signal namespace objects
    * improves type safety in signal handling
    * updates signal registration with proper types

--- invenio_base/signals.py
+++ invenio_base/signals.py
@@ -2,17 +2,19 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2017-2018 CERN.
-#
+# Copyright (C) 2025 TUGRAZ.
+
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Signals for application creation."""
 
 from blinker import Namespace
+from blinker.base import Signal
 
-_signals = Namespace()
+_signals: Namespace = Namespace()
 
-app_created = _signals.signal("app-created")
+app_created: Signal = _signals.signal("app-created")
 """Signal sent when the base Flask application have been created.
 
 Parameters:
@@ -27,7 +29,7 @@ Example receiver:
        # ...
 """
 
-app_loaded = _signals.signal("app-loaded")
+app_loaded: Signal = _signals.signal("app-loaded")
 """Signal sent when the Flask application have been fully loaded.
 
 Parameters:
