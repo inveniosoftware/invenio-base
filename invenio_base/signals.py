dd00fbadd80e91324e8c937bab809abee59b2d3a
Laura <lthaci@tugraz.at>
2025-04-08 13:51:13 +0200

    add: tests checking type annotations
    * ensures type safety across the codebase

--- invenio_base/signals.py
+++ invenio_base/signals.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2017-2018 CERN.
-# Copyright (C) 2025 TUGRAZ.
+# Copyright (C) 2025 Graz University of Technology.
 
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
